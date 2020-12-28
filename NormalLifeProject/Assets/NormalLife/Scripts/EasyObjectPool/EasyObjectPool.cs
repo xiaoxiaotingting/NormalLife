4847e84ddba71e3ec120e7a7618779e75e769be6
戴恩斌 <1225716858@qq.com>
2020-12-28 21:06:10 +0800

    Update Third Plugins

---
+++ NormalLifeProject/Assets/NormalLife/Scripts/EasyObjectPool/EasyObjectPool.cs
@@ -0,0 +1,214 @@
+﻿/* 
+ * Unless otherwise licensed, this file cannot be copied or redistributed in any format without the explicit consent of the author.
+ * (c) Preet Kamal Singh Minhas, http://marchingbytes.com
+ * contact@marchingbytes.com
+ */
+// modified version by Kanglai Qian
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace SG
+{
+    [DisallowMultipleComponent]
+    [AddComponentMenu("")]
+    public class PoolObject : MonoBehaviour
+    {
+        public string poolName;
+        //defines whether the object is waiting in pool or is in use
+        public bool isPooled;
+        public float fLastUsedTime = 0;
+    }
+
+    public enum PoolInflationType
+    {
+        /// When a dynamic pool inflates, add one to the pool.
+        INCREMENT,
+        /// When a dynamic pool inflates, double the size of the pool
+        DOUBLE
+    }
+
+    class Pool
+    {
+        //private Stack<PoolObject> availableObjStack = new Stack<PoolObject>();
+        private List<PoolObject> availableObjList = new List<PoolObject>();
+        //the root obj for unused obj
+        private GameObject rootObj;
+        private PoolInflationType inflationType;
+        private string poolName;
+        private int objectsInUse = 0;
+        public bool bDel = false;
+
+        public Pool(string poolName, GameObject poolObjectPrefab, GameObject rootPoolObj, int initialCount, PoolInflationType type)
+        {
+            if (poolObjectPrefab == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("[ObjPoolManager] null pool object prefab !");
+#endif
+                return;
+            }
+            this.poolName = poolName;
+            this.inflationType = type;
+            this.rootObj = new GameObject(poolName + "Pool");
+            this.rootObj.transform.SetParent(rootPoolObj.transform, false);
+
+            // In case the origin one is Destroyed, we should keep at least one
+            // 不持有原始GameObject的引用，据官方说，原始Asset会有一个内部Remap结构引用，当加载的资源太多时，这个结构可能会达到10MB
+            GameObject go = GameObject.Instantiate(poolObjectPrefab);
+            PoolObject po = go.GetComponent<PoolObject>();
+            if (po == null)
+            {
+                po = go.AddComponent<PoolObject>();
+            }
+            po.poolName = poolName;
+            AddObjectToPool(po);
+
+            //populate the pool
+            populatePool(Mathf.Max(initialCount, 1));
+        }
+
+        //o(1)
+        private void AddObjectToPool(PoolObject po)
+        {
+            if(po.gameObject == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError(string.Format("return a null gameobject : {0} ", poolName));
+#endif
+                return;
+            }
+            if(rootObj == null)
+            {
+                return;
+            }
+            //add to pool
+            //po.gameObject.SetActive(false);
+            po.gameObject.name = poolName;
+            availableObjList.Add(po);
+            po.isPooled = true;
+            po.fLastUsedTime = Time.time;
+            //add to a root obj
+            po.gameObject.transform.SetParent(rootObj.transform, false);
+        }
+
+        private void populatePool(int initialCount)
+        {
+            for (int index = 0; index < initialCount; index++)
+            {
+                PoolObject po = GameObject.Instantiate(availableObjList[0]);
+                AddObjectToPool(po);
+            }
+        }
+
+        //o(1)
+        public GameObject NextAvailableObject(bool autoActive)
+        {
+            PoolObject po = null;
+            if (availableObjList.Count > 1)
+            {
+                po = availableObjList[availableObjList.Count - 1];
+                availableObjList.RemoveAt(availableObjList.Count - 1);
+            }
+            else
+            {
+                int increaseSize = 0;
+                //increment size var, this is for info purpose only
+                if (inflationType == PoolInflationType.INCREMENT)
+                {
+                    increaseSize = 1;
+                }
+                else if (inflationType == PoolInflationType.DOUBLE)
+                {
+                    increaseSize = availableObjList.Count + Mathf.Max(objectsInUse, 0);
+                }
+#if UNITY_EDITOR
+                Debug.Log(string.Format("Growing pool {0}: {1} populated", poolName, increaseSize));
+#endif
+                if (increaseSize > 0)
+                {
+                    populatePool(increaseSize);
+                    po = availableObjList[availableObjList.Count - 1];
+                    availableObjList.RemoveAt(availableObjList.Count - 1);
+                }
+            }
+
+            GameObject result = null;
+            if (po != null)
+            {
+                objectsInUse++;
+                po.isPooled = false;
+                po.fLastUsedTime = float.MaxValue;
+                result = po.gameObject;
+                //if (autoActive)
+                //{
+                //    result.SetActive(true);
+                //}
+            }
+
+            return result;
+        }
+
+        //o(1)
+        public void ReturnObjectToPool(PoolObject po)
+        {
+            if (poolName.Equals(po.poolName))
+            {
+                /* we could have used availableObjStack.Contains(po) to check if this object is in pool.
+                 * While that would have been more robust, it would have made this method O(n) 
+                 */
+                if (po.isPooled)
+                {
+#if UNITY_EDITOR
+                    Debug.LogError(po.gameObject.name + " is already in pool. Why are you trying to return it again? Check usage.");
+#endif
+                }
+                else
+                {
+                    objectsInUse--;
+                    AddObjectToPool(po);
+                }
+            }
+            else
+            {
+                Debug.LogError(string.Format("Trying to add object to incorrect pool {0} {1}", po.poolName, poolName));
+            }
+        }
+
+
+        public void ReleaseAllObjects()
+        {
+            for (int i = 0; i < availableObjList.Count; i++)
+            {
+                PoolObject po = availableObjList[i];
+                if(po && po.gameObject)
+                {
+                    Object.Destroy(po.gameObject);
+                }
+            }
+            if(rootObj != null)
+            {
+                Object.Destroy(rootObj);
+            }
+            availableObjList.Clear();
+            objectsInUse = 0;
+        }
+
+
+        public void ReleaseObjectSmart(float fInterval)
+        {
+            for (int i = availableObjList.Count - 1; i >= 1; i--)
+            {
+                PoolObject po = availableObjList[i];
+                if (po != null && Time.time - po.fLastUsedTime >= fInterval && po.gameObject != null && po.isPooled)
+                {
+                    Object.Destroy(po.gameObject);
+                    availableObjList.RemoveAt(i);
+                }
+            }
+            if(availableObjList.Count == 1 && objectsInUse == 0)
+            {
+                bDel = true;
+            }
+        }
+    }
+}
---
+++ NormalLifeProject/Assets/NormalLife/Scripts/EasyObjectPool/ResourceManager.cs
@@ -0,0 +1,256 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace SG
+{
+    [DisallowMultipleComponent]
+    //[AddComponentMenu("")]
+    public class ResourceManager : MonoBehaviour
+    {
+        //obj pool
+        private Dictionary<string, Pool> poolDict = new Dictionary<string, Pool>();
+
+        private static ResourceManager mInstance = null;
+
+        public static bool IsDestroy = false;
+
+        public static ResourceManager Instance
+        {
+            get
+            {
+                if (mInstance == null)
+                {
+                    GameObject go = new GameObject("ResourceManager", typeof(ResourceManager));
+                    go.transform.localPosition = new Vector3(4000, 0, 0);
+                    var canvasGroup = go.AddComponent<CanvasGroup>();
+                    canvasGroup.alpha = 0;
+                    //canvasGroup.interactable = false;
+                    //canvasGroup.blocksRaycasts = false;
+                    // Kanglai: if we have `GO.hideFlags |= HideFlags.DontSave;`, we will encounter Destroy problem when exit playing
+                    // However we should keep using this in Play mode only!
+                    mInstance = go.GetComponent<ResourceManager>();
+
+                    if (Application.isPlaying)
+                    {
+                        DontDestroyOnLoad(mInstance.gameObject);
+                    }
+                    else
+                    {
+                        Debug.LogWarning("[ResourceManager] You'd better ignore ResourceManager in Editor mode");
+                    }
+                }
+                return mInstance;
+            }
+        }
+        public void InitPool(string poolName, int size, PoolInflationType type = PoolInflationType.DOUBLE)
+        {
+            if (poolDict.ContainsKey(poolName))
+            {
+                return;
+            }
+            else
+            {
+                GameObject pb = Resources.Load<GameObject>(poolName);
+                if (pb == null)
+                {
+                    Debug.LogError("[ResourceManager] Invalide prefab name for pooling :" + poolName);
+                    return;
+                }
+                poolDict[poolName] = new Pool(poolName, pb, gameObject, size, type);
+            }
+        }
+
+        public void DelPool(string poolName)
+        {
+            if (!poolDict.ContainsKey(poolName))
+            {
+                return;
+            }
+            Pool pool = poolDict[poolName];
+            pool.ReleaseAllObjects();
+            poolDict.Remove(poolName);
+        }
+        /// <summary>
+        /// 通过prefab初始化
+        /// 扩展by qingqing-zhao
+        /// </summary>
+        /// <param name="poolPrefab"></param>
+        /// <param name="size"></param>
+        /// <param name="type"></param>
+        public void InitPool(GameObject poolPrefab, int size, PoolInflationType type = PoolInflationType.DOUBLE)
+        {
+            if (poolPrefab == null) return;
+            var poolName = poolPrefab.name;
+            if (!poolDict.ContainsKey(poolName))
+            {
+                poolDict[poolName] = new Pool(poolName, poolPrefab, gameObject, size, type);
+            }
+        }
+        /// <summary>
+        /// Returns an available object from the pool 
+        /// OR null in case the pool does not have any object available & can grow size is false.
+        /// </summary>
+        /// <param name="poolName"></param>
+        /// <returns></returns>
+        public GameObject GetObjectFromPool(string poolName, bool autoActive = true, int autoCreate = 1)
+        {
+            GameObject result = null;
+
+            if (!poolDict.ContainsKey(poolName) && autoCreate > 0)
+            {
+                InitPool(poolName, autoCreate, PoolInflationType.INCREMENT);
+            }
+
+            if (poolDict.ContainsKey(poolName))
+            {
+                Pool pool = poolDict[poolName];
+                result = pool.NextAvailableObject(autoActive);
+                //scenario when no available object is found in pool
+#if DEBUG
+                if (result == null)
+                {
+                    Debug.LogWarning("[ResourceManager]:No object available in " + poolName);
+                }
+#endif
+            }
+#if DEBUG
+            else
+            {
+                Debug.LogWarning("[ResourceManager]:Invalid pool name specified: " + poolName);
+            }
+#endif
+            return result;
+        }
+        /// <summary>
+        /// Returns an available object from the pool 
+        /// can use gameobject
+        /// </summary>
+        /// <param name="poolObj"></param>
+        /// <param name="autoActive"></param>
+        /// <param name="autoCreate"></param>
+        /// <returns></returns>
+        public GameObject GetObjectFromPool(GameObject poolObj, bool autoActive = true, int autoCreate = 1)
+        {
+            GameObject result = null;
+            var poolName = poolObj.name;
+            if (!poolDict.ContainsKey(poolName) && autoCreate > 0)
+            {
+                InitPool(poolObj, autoCreate, PoolInflationType.INCREMENT);
+            }
+            if (poolDict.ContainsKey(poolName))
+            {
+                Pool pool = poolDict[poolName];
+                result = pool.NextAvailableObject(autoActive);
+                //scenario when no available object is found in pool
+#if DEBUG
+                if (result == null)
+                {
+                    Debug.LogWarning("[ResourceManager]:No object available in " + poolName);
+                }
+#endif
+            }
+#if DEBUG
+            else
+            {
+                Debug.LogError("[ResourceManager]:Invalid pool name specified: " + poolName);
+            }
+#endif
+            return result;
+        }
+
+        /// <summary>
+        /// Return obj to the pool
+        /// </summary>
+        /// <param name="go"></param>
+        public void ReturnObjectToPool(GameObject go)
+        {
+            if(go == null)
+            {
+                return;
+            }
+            PoolObject po = go.GetComponent<PoolObject>();
+            if (po == null || poolDict == null)
+            {
+                GameObject.Destroy(go);
+#if DEBUG
+                if(po == null)
+                {
+                    Debug.LogWarning("Specified object is not a pooled instance: " + go.name);
+                }
+                if(poolDict == null)
+                {
+                    Debug.LogWarning("obj pool is null,please check." + go.name);
+                }
+#endif
+            }
+            else
+            {
+                Pool pool = null;
+                if (poolDict.TryGetValue(po.poolName, out pool))
+                {
+                    pool.ReturnObjectToPool(po);
+                }
+
+                else
+                {
+                    GameObject.Destroy(go);
+#if DEBUG
+                    Debug.LogWarning("No pool available with name: " + po.poolName);
+#endif
+                }
+            }
+        }
+
+        /// <summary>
+        /// Return obj to the pool
+        /// </summary>
+        /// <param name="t"></param>
+        public void ReturnTransformToPool(Transform t)
+        {
+            if (t == null)
+            {
+#if DEBUG
+                Debug.LogError("[ResourceManager] try to return a null transform to pool!");
+#endif
+                return;
+            }
+            ReturnObjectToPool(t.gameObject);
+        }
+
+        public void ReleaseAllPool()
+        {
+            foreach (var pool in poolDict.Values)
+            {
+                pool.ReleaseAllObjects();
+            }
+            poolDict.Clear();
+            poolDict = null;
+            Destroy(mInstance.gameObject);
+            mInstance = null;
+            IsDestroy = true;
+        }
+        List<string> delList = new List<string>();
+        //删除超过 fInterval时间不用的缓存 
+        public void ReleasePoolSmart(float fInterval)
+        {
+            delList.Clear();
+            if(poolDict != null)
+            {
+                foreach (var key in poolDict.Keys)
+                {
+                    Pool pool = poolDict[key];
+                    pool.ReleaseObjectSmart(fInterval);
+                    if (pool.bDel)
+                    {
+                        delList.Add(key);
+                    }
+                }
+            }
+
+            foreach (var key in delList)
+            {
+                DelPool(key);
+            }
+        }
+    }
+}
